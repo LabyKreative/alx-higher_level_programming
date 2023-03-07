4237e1b637f8d1e23db9cd8a08f9fc4278eb73af
LabyKreative <labykreative@yahoo.com>
2023-03-06 17:59:54 -0800

    Create a new sentence.

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split()[5][:-1] + " " + str.split()[8][:-1]
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
