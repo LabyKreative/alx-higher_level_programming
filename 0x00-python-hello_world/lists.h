55faa86132ea8de4c5e10ab85dfe30121b27cc67
LabyKreative <labykreative@yahoo.com>
2023-03-06 18:36:14 -0800

    Linked list cysle!

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,20 +3,24 @@
 /**
  * check_cycle - a function that checks if a singly linked list has a cycle in it.
  * @list: pointer to the beginning of the node.
- * Return: 0 if no cycle, 1 if there is a cycle
+ * Return: 0 if no cycle, 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list;
+	listint_t *current, *check;
 
-	while (fast && fast->next)
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+		&& check->next->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-		{
+		if (current == check)
 			return (1);
-		}
+		current = current->next;
+		check = check->next->next;
 	}
 	return (0);
 }
