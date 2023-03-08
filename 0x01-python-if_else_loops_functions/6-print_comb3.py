e8ec23f96f0f821822b7326f52e972c30809e413
LabyKreative <labykreative@yahoo.com>
2023-03-07 21:57:18 -0800

    Inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100, 1):
-    f = int(i / 10)
-    l = int(i % 10)
-    if f == l:
-        continue
-    if f > l:
-        continue
-    if i == 89:
-        print("{}{}".format(f, l))
-        break
-    print("{}{}, ".format(f, l), end="")
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
