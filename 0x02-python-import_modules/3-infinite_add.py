71b80dfb50f816afaa0aa9d57229bc5a2cbc635b
LabyKreative <labykreative@yahoo.com>
2023-03-09 11:33:34 -0800

    Infinite addition...

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    l = sys.argv
-    sum = 0
-    for i in range(1, len(l)):
-        sum += int(l[i])
-    print("{}".format(sum))
+    from calculator_1 import add, sub, mul, div
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if args[2] != '+' and args[2] != '-' and args[2] != '*' and args[2] != '/':
+        print("Invalid operator. Choose from: +, -, * and /")
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] == '+':
+        print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
+    elif args[2] == '-':
+        print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
+    elif args[2] == '*':
+        print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
+    elif args[2] == '/':
+        print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
