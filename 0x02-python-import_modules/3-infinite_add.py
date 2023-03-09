9b1fdbd82d2fc7b52671f75d48b5079f6e763b1b
LabyKreative <labykreative@yahoo.com>
2023-03-09 11:39:22 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ if __name__ == '__main__':
     import sys
     l = sys.argv
     size = len(l) - 1
-    ar = "argument" if size == 1 else "arguments"
-    dot = "" if size == 0 else ":"
-    print("{} {}{}".format(size, ar, dot))
+    ar = "argument" if size is 1 else "arguments"
+    dot = "." if size is 0 else ":"
+    print("{} {}".format(size, ar + dot))
     for i in range(1, size + 1):
         print("{}: {}".format(i, l[i]))
