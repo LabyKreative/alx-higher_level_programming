1d02b4ae0b7e93454b8b15f09f82de6e0cd4b867
LabyKreative <labykreative@yahoo.com>
2023-03-09 11:30:33 -0800

    How to make a script dynamicgit add .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    num_args = len(sys.argv) - 1
-    if num_args == 0:
-        print("Number of argument(s): 0.")
-        print(".")
-    else:
-        print("Number of argument(s): {}.".format(num_args))
-        print("Argument{}:".format("" if num_args == 1 else "s"))
-        for i in range(num_args):
-            print("{}: {}".format(i+1, sys.argv[i+1]))
+    args = len(sys.argv)
+    print("{} ".format(args - 1), end="")
+    if args == 1:
+        print("arguments.")
+    elif args > 1:
+        if args == 2:
+            print("argument:")
+        elif args > 2:
+            print("arguments:")
+        for i in range(args - 1):
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
