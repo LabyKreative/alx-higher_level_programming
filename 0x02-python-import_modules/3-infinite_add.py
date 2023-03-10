5d8cddc6e7594b88da62743cdce32b6aa9d9a865
LabyKreative <labykreative@yahoo.com>
2023-03-09 17:53:08 -0800

    Infinite addition....

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     import sys
     l = sys.argv
     sum = 0
-    for i in range(1, len(l)):
-        sum += int(l[i])
+    for i in range(len(l) - 1):
+        sum += int(l[i + 1])
     print("{}".format(sum))
