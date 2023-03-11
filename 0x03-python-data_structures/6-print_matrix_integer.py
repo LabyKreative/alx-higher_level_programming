c50dd9adbfa9d3b10d885f961482149640e4c746
LabyKreative <labykreative@yahoo.com>
2023-03-11 12:00:39 -0800

    Lists of all lists, Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+    x = len(matrix)
+    if not matrix[0]:
         print()
+    for j in range(x):
+        y = len(matrix[j])
+        for i in range(y):
+            if i == y - 1:
+                print("{:d}".format(matrix[j][i]))
+            else:
+                print("{:d} ".format(matrix[j][i]), end="")
