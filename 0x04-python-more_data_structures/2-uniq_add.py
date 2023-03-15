213d953a48963680bf9b413ffbaca4b2f8d7b775
LabyKreative <labykreative@yahoo.com>
2023-03-15 13:06:52 -0700

    Unique addition.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_interger = 0
-    for num in dict.fromkeys(my_list):
-        uniqu_integer += n
-    return unique_integer
+    s = 0
+    for n in dict.fromkeys(my_list):
+        s += n
+    return s
