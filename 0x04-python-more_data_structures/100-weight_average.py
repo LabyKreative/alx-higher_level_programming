ffef86b93f41c7fd92dc2ec7234b34bfe97ff23f
LabyKreative <labykreative@yahoo.com>
2023-03-15 14:31:06 -0700

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    total_weighted_sum = 0
+    total_weight = 0
+    for score, weight in my_list:
+        total_weighted_sum += score * weight
+        total_weight += weight
+    return total_weighted_sum / total_weight
