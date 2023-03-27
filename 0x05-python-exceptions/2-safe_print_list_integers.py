588c987d2abdc89ac3f564d4fbe076c869609f1f
LabyKreative <labykreative@yahoo.com>
2023-03-27 07:59:28 -0700

    Refined print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-'''def safe_print_list_integers(my_list=[], x=0):
-    num_integers_printed = 0
-    for elem in my_list[:x]:
-        try:
-            integer = int(elem)
-            print("{:d}".format(integer), end="")
-            num_integers_printed += 1
-        except (ValueError, TypeError):
-            pass
-    print("")
-    return num_integers_printed'''
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(count, x):
