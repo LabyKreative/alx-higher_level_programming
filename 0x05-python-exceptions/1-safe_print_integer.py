7eb5def3a5636df8c01f396ddf9b3139f1a8d449
LabyKreative <labykreative@yahoo.com>
2023-03-27 04:31:28 -0700

    Print and count integers!

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
         return True
+    except:
+        return False
