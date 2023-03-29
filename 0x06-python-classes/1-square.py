6fc12c7a6ddb10992d47900cf13019a64de7badd
LabyKreative <labykreative@yahoo.com>
2023-03-28 18:18:35 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Define a class of square'''
+
+
+class Square:
+    '''Square functions come in here'''
+
+    def __init__(self, size):
+        '''New square gets initalized'''
+        self.__size = size
