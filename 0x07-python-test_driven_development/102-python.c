a1a10fa5a79577a8478298476e58bf37b778634f
LabyKreative <labykreative@yahoo.com>
2023-04-03 08:17:06 -0700

    CPython #3: Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,7 +1,8 @@
 #include "Python.h"
 
 /**
- * print_python_string - Prints information about Python strings.
+ * print_python_string - a function that prints
+ * information about Python strings.
  * @p: A PyObject string object.
  */
 void print_python_string(PyObject *p)
