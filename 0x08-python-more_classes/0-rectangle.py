aa662830a8f88712955e666787cbb35767badd7e
LabyKreative <labykreative@yahoo.com>
2023-04-03 15:35:33 -0700

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    The Rectangle class represents a rectangle object.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+
+    Methods:
+        area(): Returns the area of the rectangle.
+        perimeter(): Returns the perimeter of the rectangle.
+    """
+    pass
