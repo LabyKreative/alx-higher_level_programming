184a47f3cec38c8ea3fecd8aedeb41486854fcf6
LabyKreative <labykreative@yahoo.com>
2023-04-03 07:18:04 -0700

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Defines a text-indentation function'''
+
+
+def text_indentation(text):
+    '''Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    d = 0
+    while d < len(text) and text[d] == ' ':
+        d += 1
+
+    while d < len(text):
+        print(text[d], end="")
+        if text[d] == "\n" or text[d] in ".?:":
+            if text[d] in ".?:":
+                print("\n")
+            d += 1
+            while d < len(text) and text[d] == ' ':
+                d += 1
+            continue
+        d += 1
