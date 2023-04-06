3526a9510038c5bc99fa9903132b593d3216166b
LabyKreative <labykreative@yahoo.com>
2023-04-06 11:19:18 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-'''Defines a matrix division function'''
+"""Defines a matrix division"""
 
 
 def matrix_divided(matrix, div):
-    '''Divide all elements of a matrix.
+    """Divide all elements of a matrix.
     Args:
-        matrix (list): A list of lists of ints or floats.
+        matrix (list): a list of lists of ints or floats.
         div (int/float): The divisor.
     Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
+        TypeError: if the matrix contains non-numbers.
+        TypeError: if the matrix contains rows of different sizes.
+        TypeError: if div is not an int or float.
         ZeroDivisionError: If div is 0.
     Returns:
         A new matrix representing the result of the division.
-    '''
+    """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
+            not all((isinstance(elm, int) or isinstance(elm, float))
+                    for elm in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
 
