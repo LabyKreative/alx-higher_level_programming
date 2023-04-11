df4ff18b88d4fd967632b91c02b77961bf0ab306
LabyKreative <labykreative@yahoo.com>
2023-04-11 08:34:22 -0700

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Returns the list of an object's attributes"""
+    return(dir(obj))
