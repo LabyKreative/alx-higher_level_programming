66499c7348ea02b61fb0d8dac679b8f06b2933f0
LabyKreative <labykreative@yahoo.com>
2023-04-11 10:31:20 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """This represents base geometry"""
+
+    def area(self):
+        """Umimplemented"""
+        raise Exception("area() is not implemented")
