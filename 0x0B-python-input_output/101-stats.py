2cc5ac14837efdac907951c16738361f2b97f4be
LabyKreative <labykreative@yahoo.com>
2023-04-12 22:00:03 -0700

    Log parsing...

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,9 +6,9 @@ def print_stats(size, status_codes):
     """This prints, reads stdin line by line and
     computes metrics accumulated metrics
     """
-    print(f"File size: {size}")
+    print("File size: {}".format(size))
     for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]})
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
@@ -16,33 +16,34 @@ if __name__ == "__main__":
 
     size = 0
     status_codes = {}
-    valid_codes = set(['200', '301', '400', '401', '403', '404', '405', '500'])
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
         for line in sys.stdin:
-            count += 1
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-            line_parts = line.split()
+            line = line.split()
 
             try:
-                size += int(line_parts[-1])
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
 
             try:
-                status_code = line_parts[-2]
-                if status_code in valid_codes:
-                    status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 0
-
-        if count > 0:
-            print_stats(size, status_codes)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
         print_stats(size, status_codes)
