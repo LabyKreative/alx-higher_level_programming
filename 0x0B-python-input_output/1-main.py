8edd823714c348f1c57f958f2ffd41bf0ee8caf2
LabyKreative <labykreative@yahoo.com>
2023-04-12 19:18:13 -0700

    Write to a file fix main

--- 0x0B-python-input_output/1-main.py
+++ 0x0B-python-input_output/1-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 write_file = __import__('1-write_file').write_file
 
-write_file("my_file_1.txt")
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
