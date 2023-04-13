a673b4e202dba616adc5929617bc271379e54cb9
LabyKreative <labykreative@yahoo.com>
2023-04-13 06:15:28 -0700

    Student to JSON with Filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""Defines a Student class"""
+"""Defines a class Student."""
 
 
 class Student:
-    """This represents a student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student"""
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Public method that retrieves a dictionary representation of a Student instance
-        """
-        if (isinstance(attrs, list) and
-                all(isinstance(x, str) for x in attrs)):
-            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
-        return self.__dict__
+        """Get a dictionary representation of the Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    new_dict[attr] = getattr(self, attr)
+            return new_dict
