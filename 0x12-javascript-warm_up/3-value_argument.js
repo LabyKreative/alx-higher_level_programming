70ef1e9d65ca19cf4018bb44c194857ca59b6724
LabyKreative <labykreative@yahoo.com>
2023-06-14 13:12:30 +0000

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argument = process.argv[2];
 if (argument === undefined) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   console.log(argument);
 }
