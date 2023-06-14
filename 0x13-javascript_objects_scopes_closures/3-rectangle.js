2da7f1441599af75ba04ae2ec70810db40f2dec5
LabyKreative <labykreative@yahoo.com>
2023-06-14 22:43:41 +0000

    Rectangle #3 fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Recangle {
+class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
       this.width = w;
@@ -17,4 +17,4 @@ class Recangle {
     }
   }
 }
-module.exports = Recangle;
+module.exports = Rectangle;
