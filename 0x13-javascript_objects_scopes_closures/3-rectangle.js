c3fcbdacfd1a9f3e3134025ccfffa4d7cd84eb6f
LabyKreative <labykreative@yahoo.com>
2023-06-14 22:37:06 +0000

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Recangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let y = '';
+      for (let z = 0; z < this.width; z++) {
+        y += 'X';
+      }
+      console.log(y);
+    }
+  }
+}
+module.exports = Recangle;
