de077d369a448c79130b2a8a8ed85b856f855197
LabyKreative <labykreative@yahoo.com>
2023-06-19 13:16:49 -0700

    All states via SQLAlchemy...

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the hbtn_0e_6_usa database.
-
-This script connects to a MySQL database using SQLAlchemy and retrieves all State objects
-from the 'states' table. The retrieved State objects are then printed in the format 'id: name'.
-
-Usage:
-    python3 script.py <username> <password> <database>
-
-Arguments:
-    <username>: The username to connect to the MySQL database.
-    <password>: The password for the MySQL user.
-    <database>: The name of the database containing the 'states' table.
-
-Example:
-    python3 script.py root password hbtn_0e_6_usa
+a script that lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
