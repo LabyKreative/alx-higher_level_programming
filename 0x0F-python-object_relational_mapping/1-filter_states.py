b65a921f40d42e442a6fa99641247f5696d1ebe3
LabyKreative <labykreative@yahoo.com>
2023-06-19 04:02:12 -0700

    Filter states_6

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,11 +22,12 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Query to retrieve all states with a name starting with N
-    cursor.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
+    query = "SELECT * \
+            FROM states \
+            WHERE CONVERT(`name` USING Latin1) \
+            COLLATE Latin1_General_CS \
+            LIKE 'N%';"
+    cursor.execute(query)
 
     # Fetch all rows from the result set
     states = cursor.fetchall()
