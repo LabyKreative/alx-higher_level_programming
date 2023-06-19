ad636ed1da7deb047e1ba71a76d07f44ced94632
LabyKreative <labykreative@yahoo.com>
2023-06-19 03:01:06 -0700

    Modify filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,29 +6,17 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
 
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Query to retrieve all states with a name starting with N
-    query = "SELECT * FROM states " \
-    "WHERE CONVERT(`name` USING Latin1) COLLATE Latin1_General_CS LIKE 'N%';"
-    cursor.execute(query)
-
-    # Fetch all rows from the result set
-    states = cursor.fetchall()
-
-    # Display the results
     for state in states:
         print(state)
