dd01a770ac283e2974512fbc8c51689e4a2ffb19
LabyKreative <labykreative@yahoo.com>
2023-07-02 16:48:00 -0700

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a script that takes in a URL and an email, sends a POST request to same,
+use same as parameter and displays the body of the response (decoded in utf-8)
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
