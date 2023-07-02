1b00036eebb5beb1ce05fa928086d9a34bc00360
LabyKreative <labykreative@yahoo.com>
2023-07-02 14:41:08 -0700

    Find a peak!

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,20 +5,18 @@
 def find_peak(list_of_integers):
     """Defines a peak in list_of_integers"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if not list_of_integers:
         return None
-        left = 0
-        right = len(list_of_integers)
-        middle = ((right - left) // 2) + left
-        middle = int(middle)
-        if right == 1:
-            return list_of_integers[0]
-        if right == 2:
-            return max(list_of_integers)
-        if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-                list_of_integers[middle] >= list_of_integers[mid + 1]:
-            return list_of_integers[middle]
-        if middle > 0 and list_of_integers[middle] < list_of_integers[middle + 1]:
-            return find_peak(list_of_integers[middle:])
-        if middle > 0 and list_of_integers[middle] < list_of_integers[middle - 1]:
-            return find_peak(list_of_integers[:middle])
+    
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        middle = (left + right) // 2
+        
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = middle + 1
+        else:
+            right = middle
+
+    return list_of_integers[left]
