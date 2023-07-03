bb516df3c61debad7ad5a11629e70f92b9e4e1d1
LabyKreative <labykreative@yahoo.com>
2023-07-03 00:06:43 -0700

    Fix whitespace

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,13 +7,13 @@ def find_peak(list_of_integers):
 
     if not list_of_integers:
         return None
-    
+
     left = 0
     right = len(list_of_integers) - 1
 
     while left < right:
         middle = (left + right) // 2
-        
+
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = middle + 1
         else:
