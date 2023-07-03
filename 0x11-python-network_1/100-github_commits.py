b78ec984b9a5fe50a8d31e317052f7d8cf54833b
LabyKreative <labykreative@yahoo.com>
2023-07-02 18:59:43 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that displays 10 most recent commits from a GitHub repo"""
+import sys
+import requests
+
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for x in range(10):
+            print("{}: {}".format(
+                commits[x].get("sha"),
+                commits[x].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
