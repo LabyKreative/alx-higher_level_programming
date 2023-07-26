063260b891bbb47180fa1d88bd6ca3822072c1f2
LabyKretive <labykretive@yahoo.com>
2023-07-26 03:49:22 -0700

    Fix quote: Lorimpsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const fs = require("fs");
-const request = require("request");
+const fs = require('fs');
+const request = require('request');
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
