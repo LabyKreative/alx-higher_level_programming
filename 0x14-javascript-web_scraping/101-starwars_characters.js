e5a56a23d8e12e2e78a167f17fedc7a30bb43ec4
LabyKretive <labykretive@yahoo.com>
2023-07-25 21:52:08 -0700

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require("request");
+const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    charactersPrint(characters, 0);
+  }
+});
+
+function charactersPrint (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        charactersPrint(characters, index + 1);
+      }
+    }
+  });
+}
