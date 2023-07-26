909e2fd0b8629a608e7c84215cf40ba4610d208e
LabyKretive <labykretive@yahoo.com>
2023-07-26 04:10:58 -0700

    Update: How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     const todos = JSON.parse(body);
-    let completed_task = {};
+    let completedTask = {};
     todos.forEach((todo) => {
-      if (todo.completed_task && completed_task[todo.userId] === undefined) {
-        completed_task[todo.userId] = 1;
-      } else if (todo.completed_task) {
-        completed_task[todo.userId] += 1;
+      if (todo.completed) {
+        if (completedTask[todo.userId] === undefined) {
+          completedTask[todo.userId] = 1;
+        } else {
+          completedTask[todo.userId] += 1;
+        }
       }
     });
-    console.log(completed_task);
+    console.log(completedTask);
+  } else {
+    console.error('Error: Unable to fetch data from the API.');
   }
 });
