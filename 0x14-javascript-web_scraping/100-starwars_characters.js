edab303ddbc9bd743af1fb5da10b4437a9d1d1e0
LabyKretive <labykretive@yahoo.com>
2023-07-26 04:20:00 -0700

    Update: Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const req = require("request");
+const req = require('request');
 const id = process.argv[2];
-const url = "https://swapi-api.alx-tools.com/api/films/"
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
-  const data_chac = data.characters;
-  for (const x of data_chac) {
+  const dataCharacter = data.characters;
+  for (const x of dataCharacter) {
     req.get(x, function (error, res, body1) {
       if (error) {
         console.log(error);
