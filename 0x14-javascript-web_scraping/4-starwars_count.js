db9bf412aa4e4c185249432ab03f7987282267cd
LabyKretive <labykretive@yahoo.com>
2023-07-26 03:47:38 -0700

    Fix quote: Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith("/18/"))
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
