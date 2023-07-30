57aed6b330a19f10ce6844cd23eb96f55c168f8b
LabyKretive <labykretive@yahoo.com>
2023-07-29 19:44:13 -0700

    Update: Star wars movie tile.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a script that prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
