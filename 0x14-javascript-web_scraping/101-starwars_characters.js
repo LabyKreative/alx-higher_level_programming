42857680ae82f72902df08399a3f5edaabc68b28
LabyKretive <labykretive@yahoo.com>
2023-07-29 19:51:44 -0700

    Update: Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a script that prints all characters of a Star Wars movie:
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
